b41f523fee65c13635122337c36aaa9c0ac9a3a3
Shiiya0418 <52955154+Shiiya0418@users.noreply.github.com>
2024-01-09 12:06:59 +0900

    update preprocess codes.

--- preprocess/midi_to_remi.py
+++ preprocess/midi_to_remi.py
@@ -9,16 +9,9 @@ from typing import Dict, List
 
 def make_REMI(tokenizer_params: Dict=None):
     config = TokenizerConfig(**tokenizer_params) if tokenizer_params is not None else TokenizerConfig()
-    tokens_path = Path('./lmd_REMI_no_bpe')
+    tokens_path = Path('./lmd_REMI')
     tokenizer = REMI(config)
     midi_paths = list(Path('./lmd_matched_flat').glob('**/*.mid'))
     tokenizer.tokenize_midi_dataset(midi_paths, tokens_path)
-
-# def make_Octuple(tokenizer_params: Dict=None):
-#     config = TokenizerConfig(**tokenizer_params) if tokenizer_params is not None else TokenizerConfig()
-#     tokens_path = Path('./lmd_Octuple_no_bpe')
-#     tokenizer = Octuple(config)
-#     midi_paths = list(Path('./lmd_matched_flat').glob('**/*.mid'))
-#     tokenizer.tokenize_midi_dataset(midi_paths, tokens_path)
 
 make_REMI()
